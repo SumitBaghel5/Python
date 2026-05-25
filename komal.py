c62de4a5dfd9d77cd0a854a7f23bf3ff3bd91ab4
SumitBaghel5 <sumitbaghel9965@gmail.com>
2026-05-26 00:09:36 +0530

    Variable practise Q

--- komal.py
+++ komal.py
@@ -1,4 +1,7 @@
-print("hl python bhos\tdike")
-ABHISHEK = 52
-abhishek = 5523
-print(ABHISHEK)
+# print("hl python bhos\tdike")
+# ABHISHEK = 52
+# abhishek = 5523
+# print(ABHISHEK)
+
+name = str(input("write your name:"))
+print(name)
