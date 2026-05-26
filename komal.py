e600e3942ff4d2fd91795f3c019dde5daf8228ef
SumitBaghel5 <sumitbaghel9965@gmail.com>
2026-05-26 23:09:46 +0530

    Delete unnecessary file

--- komal.py
+++
@@ -1,7 +0,0 @@
-# print("hl python bhos\tdike")
-# ABHISHEK = 52
-# abhishek = 5523
-# print(ABHISHEK)
-
-name = str(input("write your name:"))
-print(name)
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-print("hl python bhos\tdike")
-# ABHISHEK = 52
-# abhishek = 5523
-# print(ABHISHEK)
