4f3ae796751515cf0b868aaee07f96ad732b45b5
SumitBaghel5 <sumitbaghel9965@gmail.com>
2026-05-30 21:46:56 +0530

    Add Range

--- rough.py
+++ rough.py
@@ -1,3 +1,4 @@
-tuple = (23.5,86,"ram",[-56,26],("apple","banana"))
-tuple [3][0] = -526
-print(tuple)
+numbers = range(1, 110, 5)
+
+print(type(numbers))  # Output: <class 'range'>
+print(list(numbers))  # Output: [1, 3, 5, 7, 9]
