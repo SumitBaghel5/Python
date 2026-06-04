45ace0d8a6c148979eb9e461331c327d69ee3324
SumitBaghel5 <115996722+SumitBaghel5@users.noreply.github.com>
2026-06-05 02:42:23 +0530

    Doing Operator Que

--- rough.py
+++ rough.py
@@ -1,6 +1,23 @@
-print ("Total Marks of Each Subject = 100")
-Physics = int(input("Marks in Physics ="))
-Chemistry = int(input("Marks in Chemistry = "))
-Maths = int(input("Marks in Maths = "))
-English = int(input("Marks in English = "))
-Physical_Education = int(input("Marks in Physical_Education = "))
+# age = int(input("Write your age = "))
+# if age <= 13:
+#     print("You are a child ")
+# elif age >13 and age <18 :
+#     print("You are a teenager")
+# elif age >18 :
+#     print("You are old")
+
+
+#     username = input("Write your username = ")
+#     password = input("Write your Password =")
+
+#     if (username == "admin" and password == "pass"):
+#         print("Login Success full")
+#     else :
+#         print("User name or password wrong XXX")
+
+n = int(input("Write your no = "))
+
+if ( n % 2 == 0):
+    print("even ")
+else:
+    print("odd")
