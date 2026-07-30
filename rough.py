19779b2446cca337c98fd5f4d72152c8d9a1f152
SumitBaghel5 <115996722+SumitBaghel5@users.noreply.github.com>
2026-07-31 01:42:53 +0530

    Function

--- rough.py
+++ rough.py
@@ -70,59 +70,100 @@
 # print(ans)
 
 # num1 = int(input("1st no = "))
-# num2 = int(input("2nd no = "))
-# num3 = int(input("3rd no = "))
-# def avg (a,b,c):
-#     x = (a+b+c)/3
-#     return x
+# # num2 = int(input("2nd no = "))
+# # num3 = int(input("3rd no = "))
+# # def avg (a,b,c):
+# #     x = (a+b+c)/3
+# #     return x
 
-# ans = avg (num1,num2,num3)
-# # print("avg of three no",ans)
-# num = int(input("Enter your no "))
-# def fac (num):
-#     sum = 1
-#     for i in range(1,num+1):
-#         sum *= i
-# print(sum)
+# # ans = avg (num1,num2,num3)
+# # # print("avg of three no",ans)
+# # num = int(input("Enter your no "))
+# # def fac (num):
+# #     sum = 1
+# #     for i in range(1,num+1):
+# #         sum *= i
+# # print(sum)
 
-# fac(23)
-# i = 1
-# while (i<=10):
-#     if (i%2 == 0):
-#         i+= 1
-#         continue
-#     print(i)
-#     i+= 1
+# # fac(23)
+# # i = 1
+# # while (i<=10):
+# #     if (i%2 == 0):
+# #         i+= 1
+# #         continue
+# #     print(i)
+# #     i+= 1
 
-# i = 10
-# while (i>=1):
-#     print("*"*i)
-#     i-=1
-# j = 1
-# while (j<=10):
-#     print("*"*j)
-#     j+=1
-# string = "hielon3225"
-# for bh in string:
-#     print(bh)
+# # i = 10
+# # while (i>=1):
+# #     print("*"*i)
+# #     i-=1
+# # j = 1
+# # while (j<=10):
+# #     print("*"*j)
+# #     j+=1
+# # string = "hielon3225"
+# # for bh in string:
+# #     print(bh)
 
-# for i in range(1,6):
-#     print("*"*i)
+# # for i in range(1,6):
+# #     print("*"*i)
 
-# calculate the avg of 3 no
-# a = int(input("1st no = "))
-# b = int(input("2nd no = "))
-# c = int(input("3rd no = "))
+# # calculate the avg of 3 no
+# # a = int(input("1st no = "))
+# # b = int(input("2nd no = "))
+# # c = int(input("3rd no = "))
 
-# def cal_avg (a,b,c):
-#     a = int(input("1st no = "))
-#     b = int(input("2nd no = "))
-#     c = int(input("3rd no = "))
+# # def cal_avg (a,b,c):
+# #     a = int(input("1st no = "))
+# #     b = int(input("2nd no = "))
+# #     c = int(input("3rd no = "))
 
-#     avg_ = ( a+b+c)/3
-#     return(avg_)
+# #     avg_ = ( a+b+c)/3
+# #     return(avg_)
 
-# cal_avg(a,b,c)
-import keyword
-for i in keyword.kwlist:
-    print(i)
+# # cal_avg(a,b,c)
+# # a = 12
+# # b = 13
+# # sum = a+b
+# # print("sum of {} & {} is {}".format(a,b,sum))
+# # print(f"avg of {a} & {b} is {(a+b)/2}")
+
+
+# marks = [89,63,52,96,75,20,52,66]
+
+# # print(marks)
+# # print(len(marks))
+
+# # print(marks[2])
+# # # 
+# # marks[2] = 96
+# # print(type(marks))
+
+# # print(marks[4:len(marks)])
+
+# # marks.append (253652)
+# # print(marks)
+
+# # # marks.insert(3,526)
+# # # print(marks)
+# # # print("="*23)
+# # # marks.sort()
+# # # print(marks)
+# # # marks.sort(reverse=True)
+# # # print(marks)
+# # print(marks)
+# def add_numbers(a, b):
+#     result = a + b
+#     return result  # Hands the calculated total back to wherever it was called
+
+# # Calling the function and saving its returned value in a variable
+# my_total = add_numbers(5, 3)
+
+# # Now you can use 'my_total' in the rest of your code!
+# print("The total is:", my_total)
+# print("Double the total is:", my_total * 2)
+# def my_function(*, name):
+#   print("Hello", name)
+
+# my_function(name = "Emil")
